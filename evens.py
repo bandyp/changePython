9ea81013b07e91a4501633806fa8c1d20078360d
Andrew Phillips <nd201175@gmail.com>
2019-05-31 09:03:43 +0000

    byo test lesson

--- evens.py
+++ evens.py
@@ -1,17 +1,16 @@
-def is_even(number):
-    return number % 2 == 0
-
 def even_number_of_evens(numbers):
-   
-    evens = 0
+    if numbers == []: 
+        return False
+    else:
+        evens = 0
+
     for n in numbers:
-        if is_even(n):
+        if n % 2 == 0:
             evens += 1
-            
     if evens == 0:
         return False
     else:
-        return is_even(evens)
+        return evens % 2 == 0
 
 assert even_number_of_evens([]) == False, "No numbers"
 assert even_number_of_evens([2]) == False, "One even number"
